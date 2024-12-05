9c34e897cfb3792d3a39f1ad66962426acbbcb4c
OmShreeVinayak20 <omshreevinayak20@gmail.com>
2024-12-05 12:26:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+// GSAP animation for the text
+gsap.from(".right-side h1", {duration: 1, x: -50, opacity: 0, ease: "power2.out"});
+gsap.from(".right-side p", {duration: 1.5, x: -50, opacity: 0, ease: "power2.out", delay: 0.5});
